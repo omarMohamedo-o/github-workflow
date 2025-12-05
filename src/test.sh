60c1fa3f0f705149942504e24e20817fa9c76bc1
OmarMohamedo-o <omarmo.abdelsalam@gmail.com>
2025-12-05 23:34:28 +0200

    node test

--- src/test.sh
+++ src/test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#src/test.sh
+# src/test.sh
 expected_output="Hello world, nice to meet you!"
-actual_output=$(node -e "console.log (require('./src/app.js)('Test'))")
+actual_output=$(node -e "console.log(require('./src/app.js')('world'))")
 
 if [ "$actual_output" == "$expected_output" ]; then
   echo "Test passed!"
