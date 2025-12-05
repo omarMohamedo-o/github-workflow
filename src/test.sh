fb62ef9b090bfb32b0e180d01706476440d66a88
OmarMohamedo-o <omarmo.abdelsalam@gmail.com>
2025-12-05 23:22:25 +0200

    node test

---
+++ src/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#src/test.sh
+expected_output="Hello world, nice to meet you!"
+actual_output=$(node -e "console.log (require('./src/app.js)('Test'))")
+
+if [ "$actual_output" == "$expected_output" ]; then
+  echo "Test passed!"
+  exit 0
+else
+  echo "Test failed! Expected '$expected_output' but got '$actual_output'"
+  exit 1
+fi
